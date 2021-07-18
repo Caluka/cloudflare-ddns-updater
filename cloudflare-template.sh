5ac13b103a12c80d1d4e75cc54fa739c65e7100f
Caluka <46251202+Caluka@users.noreply.github.com>
2021-07-18 14:50:51 -0400

    Remove some log writes

--- cloudflare-template.sh
+++ cloudflare-template.sh
@@ -7,7 +7,7 @@ record_name=""                                     # Which record you want to be
 proxy=true                                         # Set the proxy to true or false 
 
 ###########################################
-## Check if we have an public IP
+## Check if we have a public IP
 ###########################################
 ip=$(curl -s https://api.ipify.org || curl -s https://ipv4.icanhazip.com/)
 if [ "${ip}" == "" ]; then 
@@ -19,11 +19,10 @@ fi
 ###########################################
 ## Seek for the A record
 ###########################################
-echo " Check Initiated" >> ~/log
 record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json")
 
 ###########################################
-## Check if the domaine has an A record
+## Check if the domain has an A record
 ###########################################
 if [[ $record == *"\"count\":0"* ]]; then
   message=" Record does not exist, perhaps create one first? (${ip} for ${record_name})"
@@ -32,13 +31,11 @@ if [[ $record == *"\"count\":0"* ]]; then
 fi
 
 ###########################################
-## Get the existing IP 
+## Get existing IP
 ###########################################
 old_ip=$(echo "$record" | grep -Po '(?<="content":")[^"]*' | head -1)
 # Compare if they're the same
 if [[ $ip == $old_ip ]]; then
-  message=" IP ($ip) for ${record_name} has not changed."
-  echo "${message}" >> ~/log
   exit 0
 fi
 
@@ -48,7 +45,7 @@ fi
 record_identifier=$(echo "$record" | grep -Po '(?<="id":")[^"]*' | head -1)
 
 ###########################################
-## Change the IP@Cloudflare using the API
+## Send update request
 ###########################################
 update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
                      -H "X-Auth-Email: $auth_email" \
